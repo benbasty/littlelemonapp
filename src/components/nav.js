d5f007d73c61683b63e90902f8768bf7463ad9e4
benbasty <benbasty@yahoo.fr>
2023-02-16 11:50:28 +0800

    css styling and breakpoints initial settings

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../assets/logo.svg'
 const nav = () => {
   return (
-    <nav className='container'>
+    <nav>
         <img src={logo} alt='logo'/>
         <ul>
             <li><a href='#home'>Home</a></li>
