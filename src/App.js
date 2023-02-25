826490351582459c19074bb9008542f65a04bec2
benbasty <benbasty@yahoo.fr>
2023-02-26 01:06:01 +0800

    about, menu, online-order pages html and css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Nav from './components/nav/nav'
 import LandingPage from './components/landingpage/landingPage';
+import Footer from './components/footer/footer'
 import About from './components/about/about';
 import Menu from './components/menu/menu';
 import BookingPage from './components/booking/BookingPage';
@@ -20,6 +21,7 @@ function App() {
         <Route path='/order-online' element = {<OrderOnline/> }/>
         <Route path='/login' element = {<Login/> }/>
       </Routes>
+      <Footer />
     </>
   )
 }
--- src/components/landingpage/landingPage.js
+++ src/components/landingpage/landingPage.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from '../../components/header/header'
 import Main from '../../components/main/main'
-import Footer from '../../components/footer/footer'
 
 const landingPage = () => {
   return (
     <>
         <Header />
         <Main />
-        <Footer />
     </>
   )
 }
