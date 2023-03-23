da9c805c05459e3805a7c7ece7c6a84603decba5
benbasty <benbasty@yahoo.fr>
2023-03-23 11:43:42 +0800

    navbar responsive media queries

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../../assets/potatoeslogo.png'
 import './nav.css';
 import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import {FaTimes, FaBars} from 'react-icons/fa';
 
 const Nav = () => {
@@ -11,27 +11,26 @@ const Nav = () => {
     navRef.current.classList.toggle("responsive_navbar");
   }
 
+
   return (
     <>
       <nav className='container'>
         <img src={logo} alt='logo'/>
-        <ul ref={navRef}>
-            <li><Link to='/patatos'>Home</Link></li>
-            <li><Link to='/patatos/about'>About</Link></li>
-            <li><Link to='/patatos/menu'>Menu</Link></li>
-            <li><Link to='/patatos/booking'>Reservations</Link></li>
-            <li><Link to='/patatos/order-online'>Order Online</Link></li>
-            <li><Link to='/patatos/login'>Login</Link></li>
-            <button className='nav-btn nav-close' onClick={showNavbar}>
-              <FaTimes />
-            </button>
-        </ul>
-        
+          <ul ref={navRef}>
+              <li><Link to='/patatos'>Home</Link></li>
+              <li><Link to='/patatos/about'>About</Link></li>
+              <li><Link to='/patatos/menu'>Menu</Link></li>
+              <li><Link to='/patatos/booking'>Reservations</Link></li>
+              <li><Link to='/patatos/order-online'>Order Online</Link></li>
+              <li><Link to='/patatos/login'>Login</Link></li>
+              <button className='nav-btn nav-close' onClick={showNavbar}>
+                <FaTimes />
+              </button>
+          </ul>
         <button className='nav-btn' onClick={showNavbar}>
           <FaBars />
         </button>
       </nav>
-
     </>
   )
 }
